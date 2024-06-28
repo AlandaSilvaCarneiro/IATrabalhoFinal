bdd0c33a6e20ba8df41054126f04e9e94d019e44
AlandaSilvaCarneiro <82735697+AlandaSilvaCarneiro@users.noreply.github.com>
2024-06-27 21:14:54 -0300

    funçao do modelo alexnet completa

--- TravalhoIA.py
+++ TravalhoIA.py
@@ -4,7 +4,6 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import f1_score
 import numpy as np
 
-# Função para criar o modelo AlexNet adaptado para CIFAR-10
 def AlexNet(input_shape, num_classes):
     model = models.Sequential()
 
@@ -15,3 +14,18 @@ def AlexNet(input_shape, num_classes):
     model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+    model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dropout(0.5))
+
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    return model
